15d3d40b4ec680e75199da0c80658f7f91a493cb
Kacper Kobyłecki <115729994+Akkg16@users.noreply.github.com>
2024-10-21 10:25:02 +0200

    LAB 4 - Update Index.js
    Funkcje getTitles() oraz getTemplate(titles, res)() przedwcześnie mogą zakończyć i zwrócić błąd jeżeli się taki pojawi.

--- node_js_laby/index.js
+++ node_js_laby/index.js
@@ -1,36 +1,44 @@
-const http = require("http");
-const path = require("path");
-const util = require("util");
-const v8 = require("v8");
-const anymatch = require('anymatch');
+const http = require('http');
+const fs = require('fs');
 
+const server = http.createServer(function (req, res)
+{
+    getTitles(res);
+}).listen(8000, "127.0.0.1");
 
-const hostname = '127.0.0.1';
-const port = 8000;
-const matchers = ['jablko', 'gruszka'];
+function getTitles(res)
+{
+    fs.readFile('./titles.json', function (err, data)
+    {
+        if (err)
+        {
+            return hadError(err, res);
+        }
+        getTemplate(JSON.parse(data.toString()), res);
+    });
+}
 
+function getTemplate(titles, res)
+{
+    fs.readFile('./template.html', function (err, data)
+    {
+        if (err)
+        {
+            return hadError(err, res);
+        }
+        formatHtml(titles, data.toString(), res);
+    });
+}
 
-const server = http.createServer(function (request, response) {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    console.log("Moduł anymatch")
-    console.log("Instalujemy go za pomocą 'npm install anymatch'");
-    console.log("Jest to prosty moduł, który pozwala nam porównać ze sobą tablice znaków" +
-        "i uzyskać wartość true/false");
-    console.log("Zaletą tego modułu jest to, że mozemy utworzyć tablicę zmiennych typu string" +
-        " i sprawdzić czy jakaś zmienna pasuje do jakiegokolwiek elememtu z naszej tablicy." +
-        " Nie musimy podawać żadnych indeksów")
-    console.log(anymatch(matchers, 'jablko'));
-    console.log(anymatch(matchers, 'jablka'));
-    console.log(anymatch(matchers, 'gruszka'));
-    console.log(anymatch(matchers, 'gruszki'));
-    response.end('Koniec!\n');
-});
+function formatHtml(titles, tmpl, res)
+{
+    let html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+}
 
-server.listen(port, hostname, () => {
-    util.log(v8.getHeapStatistics());
-    console.log(path.basename(__filename));
-    util.log(path.join(__dirname, 'uploads','images'));
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+function hadError(err, res)
+{
+    console.error(err);
+    res.end('Error');
+}
