8a500231dc7857e692676699e99c3cf8a1d69e56
Kacper Kobyłecki <20454@student.ans-elblag.pl>
2024-11-18 17:12:46 +0100

    Laboratorium 7 | Part 9

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Kacper Kobyłecki 20454 Grupa 1
-https://akkg16.github.io/aplikacje-internetowe/KacperKoby%C5%82eckiLabJS5/
+"Kacper Kobyłecki # Aplikacje Internetowe" 
--- lab7_react/src/AppContext.js
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from "react";
-
-export const AppContext = createContext();
-
-export function AppProvider({ children }) {
-	const value = { message: "Witaj w kontekście React!" };
-	return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
-}
