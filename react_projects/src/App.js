55eeaf2841e7d98b0c7f26f3c3c9a21363519860
Kacper Kobyłecki <20454@student.ans-elblag.pl>
2024-11-18 17:18:33 +0100

    Laboratorium 7 | Part 10

---
+++ react_projects/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Lab1 from './labs/Lab7';
+import Lab2 from './labs/Lab2';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>React Projects</h1>
+        <nav>
+          <ul>
+            <li><Link to="/lab1">Laboratorium 1</Link></li>
+            {/* Dodaj kolejne linki do laboratoriów */}
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/lab7" element={<Lab7 />} />
+          {/* Dodaj kolejne trasy */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ react_projects/src/labs/Lab7/index.js
@@ -0,0 +1,8 @@
+// src/labs/Lab7/index.js
+import React from 'react';
+
+function Lab1() {
+  return <div>Treść Laboratorium 7</div>;
+}
+
+export default Lab7;
