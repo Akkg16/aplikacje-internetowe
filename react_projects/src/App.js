7fd5af1b817a207dbba956c7fa68b8b235ee35cf
Kacper Kobyłecki <20454@student.ans-elblag.pl>
2024-11-18 18:06:07 +0100

    Laboratorium 7 | Part 14

--- react_projects/src/App.js
+++ react_projects/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Lab7 from "./labs/Lab7";
+import Lab7 from "./labs/Lab7"; // Ścieżka do komponentu dla Lab7
 
 function App() {
 	return (
-		<Router>
+		<Router basename="/aplikacje-internetowe/react_projects">
+			{" "}
+			{/* Dodajemy basename */}
 			<div>
 				<h1>React Projects</h1>
 				<nav>
@@ -12,12 +14,15 @@ function App() {
 						<li>
 							<Link to="/lab1">Laboratorium 1</Link>
 						</li>
-						{/* Dodaj kolejne linki do laboratoriów */}
+						<li>
+							<Link to="/lab7">Laboratorium 7</Link>
+						</li>
+						{/* Dodaj inne linki do laboratoriów */}
 					</ul>
 				</nav>
 				<Routes>
 					<Route path="/lab7" element={<Lab7 />} />
-					{/* Dodaj kolejne trasy */}
+					{/* Dodaj inne trasy dla laboratoriów */}
 				</Routes>
 			</div>
 		</Router>
