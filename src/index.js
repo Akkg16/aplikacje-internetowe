30d0ddc486c489af9bc2ad5f7a4a8d8d495d21bf
Kacper Kobyłecki <20454@student.ans-elblag.pl>
2024-11-21 20:27:15 +0100

    Laboratorium 7 i 8 | Part 8

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/aplikacje-internetowe">
-  <App />
-</Router>
-
+	<HashRouter>
+		<App />
+	</HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
